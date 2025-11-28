ee59be0408c86f6b0ee779f51bc4848c01fb6bad
artarona <artaronacorreo@gmail.com>
2025-11-28 20:18:32 -0300

    Delete verificar_db.py

--- verificar_db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("propiedades.db")
-cur = conn.cursor()
-cur.execute("SELECT COUNT(*) FROM properties")
-print("Cantidad de propiedades cargadas:", cur.fetchone()[0])
-conn.close()
