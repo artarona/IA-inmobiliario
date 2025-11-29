a0a41b5c828311da50e1cd7959e72733b87b1a3b
artarona <artaronacorreo@gmail.com>
2025-11-29 12:01:11 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -236,4 +236,4 @@ def status():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)  # reload=False en producción
